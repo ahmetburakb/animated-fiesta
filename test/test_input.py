1ff56f071fdc1c2d7fb85fb14b94a3ede9cf6c77
burak.bilgic <ahmetburak988@gmail.com>
2023-02-11 01:03:55 +0300

    add testing for input

--- test/test_input.py
+++ test/test_input.py
@@ -1,3 +1,24 @@
 import pandas as pd
 
-df = pd.read_csv("../messy_input.txt", sep=" ")
+def test_size():
+    df = pd.read_csv("../messy_input.csv", index_col=0)
+    assert df.shape[0] == 225
+    assert df.shape[1] == 31
+
+def test_world_pop():
+
+    df = pd.read_csv("../messy_input.csv", index_col=0)
+
+    def recode_population(population):
+        result = ""
+        try:
+            result = float(population)
+        except:
+            result = "NaN"
+        return result
+
+    df['2010'] = df['2010'].apply(recode_population)
+    df['2010'] = df['2010'].astype(float)
+    world_pop_2010 = df['2010'].sum()
+    
+    assert int(world_pop_2010) == 7065
